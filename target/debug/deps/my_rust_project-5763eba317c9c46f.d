ccf98e06316aa87220b42e1f9ed11b3c70940757
Yukyung Kang <kuarl0504@gmail.com>
2025-02-26 17:03:30 +0900

    첫 번째 Rust 프로젝트 커밋

---
+++ target/debug/deps/my_rust_project-5763eba317c9c46f.d
@@ -0,0 +1,5 @@
+/Users/yukyung.kang/Rust_Study-1/my_rust_project/target/debug/deps/libmy_rust_project-5763eba317c9c46f.rmeta: src/main.rs
+
+/Users/yukyung.kang/Rust_Study-1/my_rust_project/target/debug/deps/my_rust_project-5763eba317c9c46f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/my_rust_project-6cd7fbaaa79a90c2.d
@@ -0,0 +1,5 @@
+/Users/yukyung.kang/Rust_Study-1/my_rust_project/target/debug/deps/libmy_rust_project-6cd7fbaaa79a90c2.rmeta: src/main.rs
+
+/Users/yukyung.kang/Rust_Study-1/my_rust_project/target/debug/deps/my_rust_project-6cd7fbaaa79a90c2.d: src/main.rs
+
+src/main.rs:
